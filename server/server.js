fed8ab4a0b23d0e817aa0ad3b6449c324b847c61
unknown <ankitdwivedi.cs@yahoo.com>
2018-09-04 21:55:01 +0530

    updated socket io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-    console.log(`Server is start on port ${port}`);
+io.on('connect', (socket)=>{
+    console.log('New user connected');
     
+    socket.on('disconnect', (socket)=>{
+        console.log('Disconnect from server');
+        
+    })
 })
-console.log(publicPath);
+
+server.listen(3000,()=>{
+    console.log(`Server is start on port ${port}`);  
+})
+
