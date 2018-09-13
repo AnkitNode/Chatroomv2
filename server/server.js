156141c943e79cb177fcf9de6f3b6d070b865b01
unknown <ankitdwivedi.cs@yahoo.com>
2018-09-13 18:16:02 +0530

    newMessge  on create message added

--- server/server.js
+++ server/server.js
@@ -14,11 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-//   socket.emit('newMessage', {
-//     from: 'John',
-//     text: 'See you then',
-//     createdAt: 123123
-//   });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to Chat Room',
+    createdAt: new Date().getTime
+  });
+  socket.broadcast.emit('newMessage',{
+      text:'New User joined',
+      from: 'Admin',
+      createdAt: new Date().getTime
+
+  })
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
